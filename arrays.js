0fa3065936e661ab97ab5fb01b51169f7253d674
Travis Lavery <travisalavery@gmail.com>
2017-11-21 01:00:23 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,15 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 
-function addElementToBeginningOfArray(array, element) {
-  var returnArray = [element, ...array]
+function addElementToEndOfArray(array, element) {
+  var returnArray = [...array, element]
   return returnArray
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return [...array, element]
+}
+
+function accessElementInArray(array, index) {
+  return array[index]
 }
