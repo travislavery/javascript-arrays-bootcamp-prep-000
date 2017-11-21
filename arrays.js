79433a642975447914731b9cb1e9f0fb7e6f49e8
Travis Lavery <travisalavery@gmail.com>
2017-11-21 00:56:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var chocolateBars = ['snickers','hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+  
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
